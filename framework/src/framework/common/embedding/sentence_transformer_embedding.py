67e8dc21eb5b97091dd490652005db4a5761248f
Superlinked Release <release@superlinked.com>
2024-08-01 10:57:38 +0000

    framework/v9.0.0

--- framework/src/framework/common/embedding/sentence_transformer_embedding.py
+++ framework/src/framework/common/embedding/sentence_transformer_embedding.py
@@ -12,8 +12,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from pathlib import Path
+
 import numpy as np
 from beartype.typing import cast
+from huggingface_hub.file_download import (  # type:ignore[import-untyped]
+    repo_folder_name,
+)
 from sentence_transformers import SentenceTransformer
 from torch import Tensor
 from typing_extensions import override
@@ -25,15 +30,44 @@ from superlinked.framework.common.interface.has_default_vector import HasDefault
 from superlinked.framework.common.interface.has_length import HasLength
 from superlinked.framework.common.space.normalization import Normalization
 
+SENTENCE_TRANSFORMERS_ORG_NAME = "sentence-transformers"
+SENTENCE_TRANSFORMERS_MODEL_DIR: Path = (
+    Path.home() / ".cache" / SENTENCE_TRANSFORMERS_ORG_NAME
+)
+
 
 class SentenceTransformerEmbedding(Embedding[str], HasLength, HasDefaultVector):
     def __init__(self, model_name: str, normalization: Normalization) -> None:
-        self.model = SentenceTransformer(model_name)
+        local_files_only = self._model_is_downloaded(model_name)
+        self.model = SentenceTransformer(
+            model_name,
+            trust_remote_code=True,
+            local_files_only=local_files_only,
+            # device="cpu",
+            cache_folder=str(SENTENCE_TRANSFORMERS_MODEL_DIR),
+        )
+        # albert-base-v1
         self.__normalization = normalization
         self.__length = self.model.get_sentence_embedding_dimension() or 0
 
+    def _model_is_downloaded(self, model_name: str) -> bool:
+        return bool(model_name) and (
+            (SENTENCE_TRANSFORMERS_MODEL_DIR / model_name).exists()
+            or self._get_model_folder_path(model_name).exists()
+        )
+
+    def _get_model_folder_path(self, model_name: str) -> Path:
+        repo_id = (
+            SENTENCE_TRANSFORMERS_ORG_NAME + "/" + model_name
+            if "/" not in model_name
+            else model_name
+        )
+        return SENTENCE_TRANSFORMERS_MODEL_DIR / repo_folder_name(
+            repo_id=repo_id, repo_type="model"
+        )
+
     def embed_multiple(self, inputs: list[str]) -> list[Vector]:
-        embeddings = self.model.encode(inputs)
+        embeddings = self.model.encode(inputs) if inputs else []
         return [self.__to_vector(embedding) for embedding in embeddings]
 
     @override
